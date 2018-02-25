4518098417b3e74bc2c8abf34d67b40c5750f5c3
hassantayyab <hassandogar9@gmail.com>
2018-02-25 10:58:04 +0500

    third commit

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const path = require("path");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const authRoutes = require("./routes/auth-routes");
-const coursesRoutes = require("./routes/courses-routes");
-const passportSetup = require("./config/passport-setup");
-const keys = require("./config/keys");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const coursesRoutes = require('./routes/courses-routes');
+const passportSetup = require('./config/passport-setup');
+const keys = require('./config/keys');
 
 // set up express app
 const app = express();
 
 // set view engine
-app.set("view engine", "js");
+app.set('view engine', 'js');
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 // connect to mongoDB
 mongoose
-  .connect("mongodb://localhost/grader", {
+  .connect('mongodb://localhost/grader', {
     useMongoClient: true
     /* other options */
   })
-  .then(() => console.log("connected to MongoDB..."))
+  .then(() => console.log('connected to MongoDB...'))
   .catch(err => console.log(err));
 //mongoose.connect(keys.mongodb.dbURI, () => {
 //console.log('');
 //});
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -48,8 +48,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // initialize api route
-app.use("/api", require("./routes/api"));
-app.use("/auth", authRoutes);
+app.use('/api', require('./routes/api'));
+app.use('/auth', authRoutes);
 //app.use('/courses', coursesRoutes);
 
 // error handling middleWare
@@ -58,12 +58,12 @@ app.use(function(err, req, res, next) {
   res.status(422).send({ error: err.message });
 });
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // listen to requests
 const PORT = process.env.PORT || 1234;
 app.listen(PORT, function() {
-  console.log("Listening on port 1234");
+  console.log('Listening on port 1234');
 });
