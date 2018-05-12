d81cff8338045796f5de429a2aa85323a400f5a8
hassantayyab <hassandogar9@gmail.com>
2018-05-13 01:00:39 +0500

    v2

---
+++ config/dev_keys.js
@@ -0,0 +1,15 @@
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID: '430821155576-k6ucokf4gvqoqhng93c47q77jjsnm5gj.apps.googleusercontent.com',
+    clientSecret: '6CTPjPVpi3WmkEERH_AMHsFJ'
+  },
+  mongodb: {
+    dbURI: 'mongodb://localhost/grader',
+    mLabURI: 'mongodb://hassantayyab:hussain99@ds149138.mlab.com:49138/grader'
+  },
+  session: {
+    cookieKey: 'angrydevelopers'
+  }
+};
